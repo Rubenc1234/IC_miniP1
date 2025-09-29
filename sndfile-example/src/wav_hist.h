a389cdd9e3f053a577237c1480b417887b007dfa
Rubenc1234 <rccardeal@ua.pt>
2025-09-29 16:21:17 +0100

    new stuff

--- sndfile-example/src/wav_hist.h
+++ sndfile-example/src/wav_hist.h
@@ -37,6 +37,7 @@ class WAVHist {
   public:
 	WAVHist(const SndfileHandle& sfh) {
 		counts.resize(sfh.channels());
+		counts_mid_side.resize(1);
 		 if(sfh.channels() == 2) {
             counts_mid_side.resize(2); // cria os mapas para MID e SIDE
         }
@@ -46,8 +47,16 @@ class WAVHist {
 		size_t n { };
 		for(auto s : samples)
 			counts[n++ % counts.size()][s]++;
+		// mono -> mid (1 canal)
+		if (counts.size() == 1)
+		{
+			for(size_t i = 0 ; i < samples.size() ; ++i) {
+				counts_mid_side[0][samples[i]/bin_coarser]++;
+			}
+		}
+		
 		// stereo -> mid + side (2 canais)
-		if(counts.size() == 2) {
+		else if(counts.size() == 2) {
 			// samples: L R L R L R ...
 			for(size_t i = 0 ; i < samples.size() ; i += 2) {
 				// calcula mid e side, com amostras L e R de samples
@@ -66,13 +75,14 @@ class WAVHist {
 			out << value << '\t' << counter << '\n';
         out.close();
 		
+		// se for mono ou stereo, mostra mid
+		std::ofstream out_mid(output_file + "_mid.txt");
+		for(auto [value, counter] : counts_mid_side[0])
+			out_mid << value << '\t' << counter << '\n';
+		out_mid.close();
+
 		// se for stereo, mostra mid e side
 		if(counts_mid_side.size() >= 2) {
-			std::ofstream out_mid(output_file + "_mid.txt");
-			for(auto [value, counter] : counts_mid_side[0])
-				out_mid << value << '\t' << counter << '\n';
-        	out_mid.close();
-
 			std::ofstream out_side(output_file + "_side.txt");
 			for(auto [value, counter] : counts_mid_side[1])
 				out_side << value << '\t' << counter << '\n';
